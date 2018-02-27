eba4bcb3c4260c2f61feb107ea8bfe00c845bf5b
tdelecourt <tdelecourt1@gmail.com>
2018-02-27 16:41:09 +0100

    Nettoyage des insert redis inutiles Ouverture des logs pour les vider

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <thread>
 #include <string>
 #include <vector>
+#include <fstream>
 #include "Redis.h"
 
 //-----------------------------
@@ -21,6 +22,11 @@
 #define PATH_IMU                "/home/pi/appli/avionic_imu/avionic_debug"
 #define PATH_LINK_EARTHSTATION  "/home/pi/appli/avionic_link_earthstation/link_debug"
 #define PATH_SHUTDOWN           "/home/pi/appli/avionic_shutdown/shutdown_debug"
+
+#define PATH_LOG_IMU_SENSEHAT   "/home/pi/log/sensehat.log"
+#define PATH_LOG_IMU_GPS        "/home/pi/log/gps.log"
+#define PATH_LOG_IMU_LINK       "/home/pi/log/link.log"
+#define PATH_LOG_THREAD         "/home/pi/log/thread.log"
 
 //-----------------------------
 //----- Namespace 	  -----
@@ -40,6 +46,25 @@ void link_thread();
 
 int main(int argc, char** argv) {
 
+    // Ouverture des différents fichiers de log
+    ofstream fileImuSenseHat(PATH_LOG_IMU_SENSEHAT, ios::out | ios::trunc);
+    ofstream fileImuGps(PATH_LOG_IMU_GPS, ios::out | ios::trunc);
+    ofstream fileImuLink(PATH_LOG_IMU_LINK, ios::out | ios::trunc);
+    ofstream fileThread(PATH_LOG_THREAD, ios::out | ios::trunc);
+
+    if (fileImuSenseHat.is_open()) {
+        fileImuSenseHat.close();
+    }
+    if (fileImuGps.is_open()) {
+        fileImuGps.close();
+    }
+    if (fileImuLink.is_open()) {
+        fileImuLink.close();
+    }
+    if (fileThread.is_open()) {
+        fileThread.close();
+    }
+
     //On initialise les données sous Redis
     Redis objRedis;
     objRedis.setDataConfig();
@@ -47,12 +72,12 @@ int main(int argc, char** argv) {
     /*
      * On initialise le reste du système
      */
-    // Lancement du Thread Diagnostic
-    std::thread t1(diagnostic_thread);
-    // Lancement du Thread Link EarthStation
-    std::thread t2(link_thread);
     // Lancement du Thread IMU
-    std::thread t3(imu_thread);
+    std::thread t1(imu_thread);
+    // Lancement du Thread Diagnostic
+    std::thread t2(diagnostic_thread);
+    // Lancement du Thread Link EarthStation
+    std::thread t3(link_thread);
 
     // On Attente la fin des Thread pour finir le programme
     t1.join();
@@ -61,7 +86,7 @@ int main(int argc, char** argv) {
 
     // On coupe l'ensemble du système
     system(PATH_SHUTDOWN);
-    
+
     return EXIT_SUCCESS;
 }
 
@@ -71,28 +96,19 @@ int main(int argc, char** argv) {
 //-----------------------------
 
 void diagnostic_thread() {
-    Redis objRedis;
+    int indexNbrExecSystem = 0;
+    bool flagContinueExecution = true;
 
-    // Vérification si la configuration autorise le lancement du module diagnostique
-    if (objRedis.getDataSimple("config_system_module_enabled_diagnostic") == "true") {
-        int indexNbrExecSystem = 0;
-        bool flagContinueExecution = true;
-        
-        // Pose d'un flag montrant que le module est encore en vie
-        objRedis.setDataSimple("proc_diag", "1");
+    // On lance la boucle d'exécution du module
+    // Celle-ci relance le module 2 fois en cas de plantage
+    while (flagContinueExecution) {
+        system(PATH_DIAGNOSTIC);
+        indexNbrExecSystem++;
 
-        // On lance la boucle d'exécution du module
-        // Celle-ci relance le module 2 fois en cas de plantage
-        while (flagContinueExecution) {
-            system(PATH_DIAGNOSTIC);
-            indexNbrExecSystem++;
-
-            if (indexNbrExecSystem == 2) {
-                flagContinueExecution = false;
-            }
+        if (indexNbrExecSystem == 2) {
+            flagContinueExecution = false;
         }
     }
-    objRedis.setDataSimple("proc_diag", "0");
 }
 
 
@@ -101,28 +117,19 @@ void diagnostic_thread() {
 //-----------------------------
 
 void imu_thread() {
-    Redis objRedis;
+    int indexNbrExecSystem = 0;
+    bool flagContinueExecution = true;
 
-    // Vérification si la configuration autorise le lancement de la centrale inertielle
-    if (objRedis.getDataSimple("config_system_module_enabled_imu") == "true") {
-        int indexNbrExecSystem = 0;
-        bool flagContinueExecution = true;
+    // On lance la boucle d'exécution du module
+    // Celle-ci relance le module 2 fois en cas de plantage
+    while (flagContinueExecution) {
+        system(PATH_IMU);
+        indexNbrExecSystem++;
 
-        // Pose d'un flag montrant que le module est encore en vie
-        objRedis.setDataSimple("proc_imu", "1");
-
-        // On lance la boucle d'exécution du module
-        // Celle-ci relance le module 2 fois en cas de plantage
-        while (flagContinueExecution) {
-            system(PATH_IMU);
-            indexNbrExecSystem++;
-
-            if (indexNbrExecSystem == 2) {
-                flagContinueExecution = false;
-            }
+        if (indexNbrExecSystem == 2) {
+            flagContinueExecution = false;
         }
     }
-    objRedis.setDataSimple("proc_imu", "0");
 }
 
 //-----------------------------
@@ -130,26 +137,17 @@ void imu_thread() {
 //-----------------------------
 
 void link_thread() {
-    Redis objRedis;
+    int indexNbrExecSystem = 0;
+    bool flagContinueExecution = true;
 
-    // Vérification si la configuration autorise le lancement de la télémétrie
-    if (objRedis.getDataSimple("config_system_module_enabled_link") == "true") {
-        int indexNbrExecSystem = 0;
-        bool flagContinueExecution = true;
+    // On lance la boucle d'exécution du module
+    // Celle-ci relance le module 2 fois en cas de plantage
+    while (flagContinueExecution) {
+        system(PATH_LINK_EARTHSTATION);
+        indexNbrExecSystem++;
 
-        // Pose d'un flag montrant que le module est encore en vie
-        objRedis.setDataSimple("proc_link", "1");
-
-        // On lance la boucle d'exécution du module
-        // Celle-ci relance le module 2 fois en cas de plantage
-        while (flagContinueExecution) {
-            system(PATH_LINK_EARTHSTATION);
-            indexNbrExecSystem++;
-
-            if (indexNbrExecSystem == 2) {
-                flagContinueExecution = false;
-            }
+        if (indexNbrExecSystem == 2) {
+            flagContinueExecution = false;
         }
     }
-    objRedis.setDataSimple("proc_link", "0");
 }
